e5db46de7110d0fcc67303bd315870d81daf928b
Lily Go <lilygo7192@gmail.com>
2023-04-18 21:59:44 -0700

    Format props.spots to render the proper text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,10 +6,24 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected}, {"day-list__item--full": props.spots === 0},)
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+
+    if (spots === 1) {
+      return `1 spot remaining`;
+    }
+
+    return `${spots} spots remaining`
+  }
+
+  const spots = formatSpots(props.spots);
+
   return (
     <li className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spots}</h3>
     </li>
   );
 }
