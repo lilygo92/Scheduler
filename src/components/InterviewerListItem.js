be3e6ed6ea07024344cfd0894d72de51e73a46f2
Lily Go <lilygo7192@gmail.com>
2023-04-19 18:19:34 -0700

    Refactor component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerListItem() {
-  return <li className="interviewers__item">
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected});
+
+  const name = (props.selected ? props.name : null);
+
+  return <li onClick = {() => props.setInterviewer(props.id)}className={interviewerClass}>
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      src={props.avatar}
+      alt={props.name}
     />
-    Sylvia Palmer
+
+    {name}
   </li>;
 }
