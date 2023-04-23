0a0f1f765ef35a57fcdad2500d9fdd717abc3ba0
Lily Go <lilygo7192@gmail.com>
2023-04-22 22:27:35 -0700

    Add getAppointmentsForDay helper function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  const result = []
+  const appointments = {...state.appointments};
+  let dayAppointments = []
+  
+  state.days.map(obj => {
+    if (obj.name === day) {
+      dayAppointments = obj.appointments;
+    }
+  });
+
+  for (const elem of Object.values(appointments)) {
+    if (dayAppointments.includes(elem.id)) {
+      result.push(elem);
+    }
+  }
+  return result;
+}
