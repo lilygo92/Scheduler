abfc178a81a2a2d68528552c31e66b8743c0a1ed
Lily Go <lilygo7192@gmail.com>
2023-04-23 16:36:47 -0700

    Implement getInterview in application

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay , getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
