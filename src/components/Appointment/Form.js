ee2d04c9219d8de07983675b9d43b198527376cc
Lily Go <lilygo7192@gmail.com>
2023-04-22 17:28:21 -0700

    Add controlled components and props

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+    return;
+  }
+
+  const cancel = function() {
+    props.onCancel();
+    return reset();
+  }
+
+  return <main className="appointment__card appointment__card--create">
+    <section className="appointment__card-left">
+      <form onSubmit={event => event.preventDefault()} autoComplete="off">
+        <input
+          className="appointment__create-input text--semi-bold"
+          name="name"
+          type="text"
+          placeholder="Enter Student Name"
+          value={student}
+          onChange={(event) => setStudent(event.target.value)}
+        />
+      </form>
+      <InterviewerList 
+        interviewers = {props.interviewers}
+        value={interviewer}
+        onChange={setInterviewer}
+      />
+    </section>
+    <section className="appointment__card-right">
+      <section className="appointment__actions">
+        <Button danger onClick={event => cancel()}>Cancel</Button>
+        <Button confirm onClick={event => props.onSave()}>Save</Button>
+      </section>
+    </section>
+  </main>
+}
