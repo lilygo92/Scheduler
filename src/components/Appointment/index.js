90a4c4b142aeedd476756348ff921a67458a7e04
Lily Go <lilygo7192@gmail.com>
2023-04-22 18:18:28 -0700

    Render empty appointment story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./styles.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-export default function Appointment() {
-  return <article className="appointment"></article>
+export default function Appointment(props) {
+  let interview = props.interview ? <Show /> : <Empty />
+
+  return <article className="appointment">
+    <Header
+      time = {props.time}
+    />
+    {interview}
+  </article>
 }
